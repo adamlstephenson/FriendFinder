e57a0a1c8672c5cbb46c30b45e1446c09cbe780d
Adam Stephenson <astephensonx@gmail.com>
2018-05-10 22:18:59 -0500

    server set up

---
+++ server.js
@@ -0,0 +1,23 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Express
+var app = express();
+
+//PORT
+var PORT = process.env.PORT || 8080;
+
+//Express Data Parsing
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+//Router
+// require("./routing/apiRoutes.js")(app);
+// require("./routing/htmlRoutes.js")(app);
+
+//Listener
+app.listen(PORT, function() {
+    console.log("app listening on PORT: " + PORT)
+});
