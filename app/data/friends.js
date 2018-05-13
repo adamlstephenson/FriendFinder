5ea47cc4953479538d2a9c23fbe1c9d86a5f3a50
Adam Stephenson <astephensonx@gmail.com>
2018-05-12 20:33:29 -0500

    a bunch more functionality added, including api/friends

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+    {
+        name: "Curtis",
+        photo: "https://treasurer.sc.gov/treasurers-office/meet-the-treasurer/",
+        scores: [
+            5,
+            3,
+            1,
+            2,
+            5,
+            4,
+            3,
+            3,
+            4,
+            1
+        ]
+    },
+];
+
+module.exports = friends;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,13 +1,20 @@
 //Dependencies
 
-
+var friends = require("../app/data/friends.js");
 
 //Routing
 
 module.exports = function (app) {
 
-    //GET Requests
+    //GET Request
 
-    //POST Requests
+    app.get("/api/friends", function (req, res) {
+        res.json(friends)
+    });
 
+    //POST Request
+
+    app.post("/api/friends", function (req, res) {
+       friends.push(req.body)
+    });
 }
