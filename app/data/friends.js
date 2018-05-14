61f09fbc02b41973cd51dc3df7a3ab0fb2c25b34
Adam Stephenson <astephensonx@gmail.com>
2018-05-13 21:58:12 -0500

    added additional dummy friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         name: "Curtis",
-        photo: "https://treasurer.sc.gov/treasurers-office/meet-the-treasurer/",
+        photo: "http://law.syr.edu/uploads/images/people-image/Kelly_Curtis_7141.jpg",
         scores: [
             5,
             3,
@@ -15,6 +15,75 @@ var friends = [
             1
         ]
     },
+
+    {
+        name: "Kate",
+        photo: "https://www.gv.com/img/team-large/kate-aronowitz.jpg",
+        scores: [
+            5,
+            3,
+            2,
+            3,
+            1,
+            1,
+            4,
+            2,
+            1,
+            1
+        ]
+    },
+
+    {
+        name: "Bryce",
+        photo: "http://www.brycedessner.com/wp-content/uploads/2014/05/Bryce-Dessner-June-2013-Credit-Charlotte-de-Mezamat.jpg",
+        scores: [
+            1,
+            3,
+            5,
+            2,
+            5,
+            3,
+            2,
+            2,
+            5,
+            1
+        ]
+    },
+
+    {
+        name: "Jenna",
+        photo: "http://mediad.publicbroadcasting.net/p/innovationtrail/files/styles/x_large/public/201308/Jenna_Flanagan.JPG",
+        scores: [
+            4,
+            1,
+            4,
+            3,
+            3,
+            3,
+            4,
+            2,
+            5,
+            1
+        ]
+    },
+
+    {
+        name: "Alice",
+        photo: "https://www.xconomy.com/wordpress/wp-content/images/2016/06/Alice-Zhang-Orig-size-15x10-1100x733.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            3,
+            3,
+            1,
+            4,
+            2,
+            3,
+            1
+        ]
+    }
+
 ];
 
 module.exports = friends;
