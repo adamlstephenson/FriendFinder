cc696ced63f2119c1330556be2a204631dc96f44
Adam Stephenson <astephensonx@gmail.com>
2018-05-13 21:47:24 -0500

    match functionality added

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -15,6 +15,53 @@ module.exports = function (app) {
     //POST Request
 
     app.post("/api/friends", function (req, res) {
-       friends.push(req.body)
+        //friends.push(req.body)
+
+        var friendMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        }
+
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        friends.push(userData)
+
+        //Total difference loop
+        //=========================================
+
+        for (var i = 0; i < friends.length; i++) {
+
+            console.log(friends[i])
+
+            totatDifference = 0;
+
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                console.log(totalDifference)
+
+                if (totalDifference <= friendMatch.friendDifference) {
+
+                    friendMatch.name = friends[i].name;
+                    friendMatch.phot = friends[i].photo;
+                    friendMatch.friendDifference = totalDifference;
+                }
+            }
+
+
+        }
+
+        res.json(friendMatch)
+
     });
+
+    //Empty arrays for testing purposes
+
+    app.post("/api/clear", function (req, res) {
+        friends = [];
+        console.log(friends)
+    })
 }
